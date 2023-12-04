72bd9b60449b3252e1faa3d5e06cda1b40499f9d
samuel21695 <samuel21695@gmail.com>
2023-12-04 14:14:35 +0900

    📝 component test

--- src/components/file2.js
+++ src/components/file2.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function File2() {
-  return <h2>File 2 Content</h2>;
+  return (
+    <div>
+      <h2>File 2 Content</h2>
+      <h3>File 2-1 Content</h3>
+    </div>
+  );
 }
 
 export default File2;
